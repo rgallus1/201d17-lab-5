5225556e4957e1eb99404e88f9e85ae6089de568
rgallus1 <roseannegallus@yahoo.com>
2016-12-02 15:47:38 -0800

    added a new branch

--- app.js
+++ app.js
@@ -39,8 +39,11 @@ function multiply(number1, number2) {
 multiply(5,9);
 
 
+
+
+
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+  testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
